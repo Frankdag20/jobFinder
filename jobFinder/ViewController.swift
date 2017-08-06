13a01efdbd4a5fcb7bfa666d2b65e1a2dd4c3afb
Frank D'Agostino <frankdagostino@franks-mbp.fios-router.home>
2017-08-06 12:46:02 -0400

    Initial Commit

---
+++ jobFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jobFinder
+//
+//  Created by Frank D'Agostino on 8/6/17.
+//  Copyright © 2017 Frank D'Agostino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
